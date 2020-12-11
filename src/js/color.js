8ef16f5a7a4b5ab270fad3f990db880e6d0ab5bf
evgenii-ivanov87 <66361345+evgenii-ivanov87@users.noreply.github.com>
2020-12-11 19:50:25 +0200

    Дз часть 1 готова
    Дз часть 1 готова

--- src/js/color.js
+++ src/js/color.js
@@ -8,30 +8,34 @@ const colors = [
 ];
 
 const refs = {
-  bodystyles: document.querySelector('body'),
-  bottunStartRef: document.querySelector('[data-action="start"]'),
-  bottunStopRef: document.querySelector('[data-action="start"]')
-  
-}
+  bodyEl : document.querySelector('body'),
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]')
+  }
 
-refs.bottunStartRef.addEventListener('click',buttonActiv)
+let intervalId = '';
+refs.stopBtn.disabled = true;
 
-console.dir(refs.bodystyles)
-console.dir(refs.bottunStartRef)
-console.dir(refs.bottunStopRef)
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
 
-function buttonNewColor() {
-  const index = randomIntegerFromInterval(0, colors.length-1)
-  const newColor = colors[index]
-  console.log(newColor)
-  refs.bodystyles.style.backgroundColor = newColor
-}
-function buttonActiv() {
-  setInterval(buttonNewColor, 2000)
-  
-}
+const switcher = () => {
+  refs.bodyEl.style.backgroundColor =
+    colors[randomIntegerFromInterval(0, colors.length - 1)];
+};
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+function onStartBtnClick() {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+  intervalId = setInterval(switcher, 1000);
+}
+
+function onStopBtnClick() {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+  clearInterval(intervalId);
+}
